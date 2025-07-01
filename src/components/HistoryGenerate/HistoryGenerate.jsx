08df7a8fcc7cbcb2c1a4cbf84f31563a8776258a
VEIDD <separik3@gmail.com>
2025-07-01 15:55:49 +0300

    deploy

--- src/components/HistoryGenerate/HistoryGenerate.jsx
+++ src/components/HistoryGenerate/HistoryGenerate.jsx
@@ -1,14 +1,13 @@
 import { GENERATE_DATA } from "../../constans";
 import { QRCodeSVG } from "qrcode.react";
 import styles from "./HistoryGenerate.module.css";
-
 export const HistoryGenerate = () => {
   const data = JSON.parse(localStorage.getItem(GENERATE_DATA) || "[]");
   return (
     <div className={styles.container}>
       <ul>
         {data.map(() => {})}
-				
+
         {data.map((text) => (
           <p key={text}>
             {text}
